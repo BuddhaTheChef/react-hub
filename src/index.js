f3194864eef8459b88746bebc795d9d9e13fdc9b
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-06-14 13:36:57 -1000

    search function workable and changed render function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 import './App.css'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -18,18 +19,25 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     }
-    YTSearch({key: API_KEY, term: 'skateboards'}, (videos) => {
+
+    this.videoSearch('huskys');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
        })
     })
   }
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 400);
     return (
       <div>
-        <SearchBar/>
-        <VideoDetail video={this.state.selectedVideo}/>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
           videos={this.state.videos}
