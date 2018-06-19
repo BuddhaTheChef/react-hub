7ae360acb6548683b5276252ab6a863f1aca27eb
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-06-18 15:59:58 -1000

    changing div layout

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,11 +10,14 @@ class SearchBar extends Component {
   }
   render() {
     return (
+      <div>
+        <h1 className="main-title">YouTube</h1>
       <div className="search-bar-main">
         <input
           value={this.state.term}
           onChange={ event => this.onInputChange(event.target.value)} />
       </div>
+    </div>
     )
   }
 
